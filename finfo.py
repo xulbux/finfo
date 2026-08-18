4a783a71052e9f9bc1719ec6c3f38c6b5a054acc
xulbux <xulbux.real@gmail.com>
2026-08-18 14:48:24 +0200

    Initial logic setup

--- finfo.py
+++ finfo.py
@@ -1,8 +1,20 @@
 from pathlib import Path
+from helpers.types import GeneralInfo
 import xulbux as xx
 
 ARGS = xx.console.get_args({"path": "before"})
 
 
+def get_general_info(path: Path) -> GeneralInfo:
+    if not path.exists():
+        raise FileNotFoundError(f"The path '{path}' does not exist.")
+
+    ...
+
+    return general_info
+
+
 if __name__ == "__main__":
     path = Path(ARGS.path.get(0, "."))
+
+    ...
